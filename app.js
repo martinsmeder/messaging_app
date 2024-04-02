0c0f9eb2caad0287f74a2ae40d2273b5cf483300
martinsmeder <119725728+martinsmeder@users.noreply.github.com>
2024-04-02 16:31:37 +0200

    Setup database

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
 const app = express();
+
+// Set up mongoose connection
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.DATABASE_URL;
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -15,7 +25,5 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-
-app.listen(3000, () => console.log("App listening on port 3000!"));
 
 module.exports = app;
